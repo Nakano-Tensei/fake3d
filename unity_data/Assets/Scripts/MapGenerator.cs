a766ae3e5ca6ba71006b4cf5143b96090c3d8fde
taisei10251 <taisei10251@gmail.com>
2024-12-15 17:30:55 +0900

    最初の追加

---
+++ unity_data/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] TextAsset mapText;
+    [SerializeField] GameObject[] prefabs;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
